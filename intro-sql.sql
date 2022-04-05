a00a841e3d7dfa9347c6f39b75f2c85ecd653be5
profalety <profalety@gmail.com>
2022-04-05 16:34:20 +0200

    add introsql

---
+++ intro-sql.sql
@@ -0,0 +1,12 @@
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+SELECT title FROM film;
+SELECT distinct  language_id FROM film;
+
+SELECT * FROM store;
+SELECT * FROM staff;
+SELECT first_name FROM staff;
+
+
+
